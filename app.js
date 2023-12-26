306c05663e274ed700e53004d1a96e341144b80c
renad <renad@gmail.com>
2023-12-26 07:52:32 +0300

    My first Asg

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-(function () {
+(function(){
 'use strict';
 
-angular.module('NameCalculator', [])
+    angular.module('module1AssignmentApp', [])
 
-.controller('NameCalculatorController', function ($scope) {
-  $scope.name = "";
-  $scope.totalValue = 0;
+    .controller('module1AssignController', function($scope, $filter){
 
-  $scope.displayNumeric = function () {
-    var totalNameValue = calculateNumericForString($scope.name);
-    $scope.totalValue = totalNameValue;
-  };
-
-
-  function calculateNumericForString(string) {
-    var totalStringValue = 0;
-    for (var i = 0; i < string.length; i++) {
-      totalStringValue += string.charCodeAt(i);
-    }
-
-    return totalStringValue;
-  }
-
-});
-
-
+        $scope.inputItem = "";
+        $scope.inputResult = "";
+        $scope.checkItems = function(){
+            var tots = 0;
+            var splitItem = $scope.inputItem.split(",");
+            console.log(splitItem.length);
+            if(splitItem.length > 3)
+            {
+                document.getElementById("resultType").style.color = "#F00";
+                $scope.inputResult = "Too much!";
+            }
+            else if($scope.inputItem == "")
+            {
+                document.getElementById("resultType").style.color = "#F00";
+                $scope.inputResult = "Please enter data first";
+            }
+            else{
+                document.getElementById("resultType").style.color = "#0F0";
+                $scope.inputResult = "Enjoy";
+            }
+        }
+    });
 })();
